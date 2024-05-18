304257839f0231cb617ec8c8dd3bc211c5fa2c44
lxyongit <58759353+lxyongit@users.noreply.github.com>
2024-05-18 22:50:38 +0800

    Add files via upload

---
+++ cdn/loopMail.js
@@ -0,0 +1,53 @@
+function consoleResult(result) {
+  console.log('result', result)
+}
+function sendMail(email, count = 1) {
+  if (count >= 2) {
+    return;
+  }
+  const script = document.createElement('script')
+  script.src =
+    `https://email-temporary.com/api/sendTempMail?email=${email}&cb=consoleResult`;
+  document.body.appendChild(script);
+  
+  setTimeout(() => script.remove(), 1000 * 30);
+  script.onerror = () => {
+    setTimeout(() => {
+      sendMail(email, count + 1)
+    }, 1000 * 10)
+  };
+}
+function saveMail(url, next) {
+  if(!url || window.preurl === url) {
+    return
+  }
+  window.preurl = url
+  const script = document.createElement('script')
+  script.src  = `https://email-temporary.com/api/saveBlink?url=${url}&cb=consoleResult`;
+  script.onerror = () => {
+    !next && saveMail(url, true)
+  }
+  document.body.appendChild(script);
+  setTimeout(() => script.remove(), 1000 * 30);
+  const w = window.open(
+    `https://pingomatic.com/ping/?title=mailtemp&blogurl=${encodeURIComponent(
+      url
+    )}&rssurl=&chk_blogs=on&chk_feedburner=on&chk_tailrank=on&chk_superfeedr=on`
+  );
+  setTimeout(() => w.close(), 1000 * 10);
+}
+
+function intervalSendMail(fn) {
+  fn()
+  return setInterval(() => fn(), 1000 * 60 * 30)
+}
+function intervalSaveUrl(fn) {
+  return setInterval(() => fn(), 1000 * 60 * 5)
+}
+
+function create(getEmail, getUrl) {
+  return async () => {
+    intervalSendMail(async () => sendMail(await getEmail()))
+    intervalSaveUrl(async () => saveMail(await getUrl()))
+  }
+}
